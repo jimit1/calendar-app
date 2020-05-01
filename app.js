3e47e85ddd7da1d1f8ee8a405b66121d595524a8
Jim Mehta <jimit1@gmail.com>
2020-04-30 17:35:22 -0700

    this is true working version

--- app.js
+++ app.js
@@ -43,41 +43,40 @@ $(document).ready(function () {
   var clock = 0;
   var amPm = "";
 
-  function loadCalendar() {
-    for (var i = 9; i < 21; i++) {
-      if (i < 13) {
-        clock = i;
-        amPm = "am";
-      } else {
-        clock = i - 12;
-        amPm = "pm";
-      }
-      if (!storeData) {
-        var storeData = [
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "",
-        ];
-      }
-      $(".container").append(`<div class="row time-block">
+  for (var i = 9; i < 21; i++) {
+    if (i < 13) {
+      clock = i;
+      amPm = "am";
+    } else {
+      clock = i - 12;
+      amPm = "pm";
+    }
+    if (!storeData) {
+      var storeData = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      ];
+    }
+    $(".container").append(`<div class="row time-block">
     <h6 class="text-center col-1 p-0">${clock} ${amPm}</h6>
     <textarea
       class="text-center col-10 textInput"
@@ -89,20 +88,19 @@ $(document).ready(function () {
     <button class="saveBtn col-1 p-0" id="${i}">Save</button>
   </div>
     `);
-    }
-
-    //For past time blocks
-    for (var p = 9; p < time; p++) {
-      $(`#t${p}`).attr("style", "background-color: #d3d3d3; color: white;");
-      // $(`#t${p}`).attr("readonly", "readonly");
-    }
-    //For future time blocks
-    for (var f = 21; f > time; f--) {
-      $(`#t${f}`).attr("style", "background-color: #77dd77; color: white;");
-    }
-    //For current time blocks
-    $(`#t${time}`).attr("style", "background-color: #ff6961; color: white;");
   }
+
+  //For past time blocks
+  for (var p = 9; p < time; p++) {
+    $(`#t${p}`).attr("style", "background-color: #d3d3d3; color: white;");
+    // $(`#t${p}`).attr("readonly", "readonly");
+  }
+  //For future time blocks
+  for (var f = 21; f > time; f--) {
+    $(`#t${f}`).attr("style", "background-color: #77dd77; color: white;");
+  }
+  //For current time blocks
+  $(`#t${time}`).attr("style", "background-color: #ff6961; color: white;");
 
   $(document).on("click", ".saveBtn", function () {
     var id = $(this).attr("id");
@@ -138,5 +136,4 @@ $(document).ready(function () {
       window.location.href = "index.html";
     }, 2000);
   });
-  loadCalendar();
 });
